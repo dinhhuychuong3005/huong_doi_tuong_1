392ccc537c8e29b644f24641d475ec593bae3487
dinhhuychuong3005 <dinhhuychuong3005@gmail.com>
2021-04-23 08:46:25 +0700

    thuc_hanh_xay_dung_lop_date.html

---
+++ dien_thoai.js
@@ -0,0 +1,13 @@
+class Mobile {
+    constructor() {
+        this.message = '';
+        this.inbox = [];
+        this.outbox = [];
+    }
+    writeMsg(msg){
+        this.message = msg;
+    }
+    receiveMsg(msg){
+        this.b
+    }
+}
