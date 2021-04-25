5335a944f01b4701408490eecf698289736042a5
dinhhuychuong3005 <dinhhuychuong3005@gmail.com>
2021-04-25 19:25:14 +0700

    pikachu.html

--- pikachhu.js
+++ pikachhu.js
@@ -1,23 +1,32 @@
-function Pikachu(image,top,left,size){
+
+function Hero(image, top, left, size){
     this.image = image;
     this.top = top;
     this.left = left;
     this.size = size;
-    this.pikachuElement = function (){
+
+    this.getHeroElement = function(){
         return '<img width="'+ this.size + '"' +
             ' height="'+ this.size + '"' +
-            ' src="' + this.image +'"' +
+            ' src="'+ this.image +'"' +
             ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
     }
-    this.moveRight = function (){
+
+    this.moveRight = function(){
         this.left += 20;
+        console.log('ok: ' + this.left);
     }
+
 }
-let pikachu = new Pikachu(,20,30,200);
-function start(){
-    if (pikachu.left < window.innerWidth - pikachu.size){
-        pikachu.moveRight();
+
+var hero = new Hero('pikachu.png', 20, 30, 200);
+console.log(hero.getHeroElement())
+function start() {
+    if (hero.left < window.innerWidth - hero.size) {
+        hero.moveRight();
     }
-    document.getElementById('game').innerHTML = pikachu.pikachuElement();
-    setInterval(start,500)
+
+    //setTimeout(start, 500)
 }
+document.getElementById('game').innerHTML = 'fdfdf'
+//start();
